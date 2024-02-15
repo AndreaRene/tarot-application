b18974c49ef043bf5da997e81cd23278de20bd55
Andrea <andrea@interhact.net>
2024-02-15 12:56:47 -0600

    added introductory Card model properties

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,7 +1,16 @@
 const { Schema, model } = require('mongoose');
 
 const cardSchema = new Schema({
-
+    name: {
+        type: String,
+    },
+    number: {
+        type: Number,
+    },
+    arcana: {
+        type: String,
+        enum: ['Major', 'Minor'],
+    } 
 });
 
 const Card = model('Card', cardSchema);
