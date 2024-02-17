fdc8dd37b63183d08397a9afde9cbcb3590a798d
AndreaRene <andrea@interhact.net>
2024-02-17 17:58:24 -0600

    add notes for queries needed in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,13 @@ const resolvers = {
 		allDecks: async () => Deck.find(),
 		oneDeck: async (_, { deckId }) => {
 			return Deck.findOne({_id: deckId})
-			}
+		}
+		//query all cards from one deck
+		//query x number of random cards from one deck
+		//query one card
+		//query one spread
+		//query all spreads
+		//query ME
 	},
 };
 
