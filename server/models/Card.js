3d887a37ca63c18f96a4556a549287e7326d3f51
AndreaRene <andrea@interhact.net>
2024-02-17 19:18:00 -0600

    card seeds complete and tested

--- server/models/Card.js
+++ server/models/Card.js
@@ -13,7 +13,7 @@ const cardSchema = new Schema({
     },
     suit: {
         type: String,
-        enum: ['Earth', 'Fire', 'Water', 'Wind']
+        enum: ['Earth', 'Fire', 'Water', 'Wind', 'Keys']
     },
     deck: {
         type: Types.ObjectId,
