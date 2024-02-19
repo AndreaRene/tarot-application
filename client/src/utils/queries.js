eead7c63dcbeb35c3e1c9952a871d018c5b8535c
Andrea <andrea@interhact.net>
2024-02-19 15:38:06 -0600

    add one deck query to frontend utils

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,27 @@
 import { gql } from '@apollo/client';
 
-export const GET_ALL_DECKS = gql`
-    query AllDecks {
-  allDecks {
-    _id
-    cards {
+export const QUERY_ALL_DECKS = gql`
+  query AllDecks {
+    allDecks {
       _id
+      cards {
+        _id
+      }
+      deckName
+      description
     }
-    deckName
-    description
   }
-}
-`
+`;
+
+export const QUERY_ONE_DECK = gql`
+  query OneDeck($deckId: ID!) {
+    oneDeck(deckId: $deckId) {
+      _id
+      cards {
+        _id
+      }
+      deckName
+      description
+    }
+  }
+`;
