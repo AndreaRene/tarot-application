1fff158ed13ba7ab8443dd33767daafe2ba69ed4
Andrea <andrea@interhact.net>
2024-02-23 14:08:03 -0600

    add fields to reading model for user, deck, spread, cards, and user notes with a title and text body

--- server/models/Reading.js
+++ server/models/Reading.js
@@ -1,7 +1,32 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+//don't forget to add to typedefs!
 
 const readingSchema = new Schema({
-
+    user: {
+        type: Types.ObjectId,
+        ref: 'User'
+    },
+    deck: {
+        type: Types.ObjectId,
+        ref: 'Deck'
+    },
+    spread: {
+        type: Types.ObjectId,
+        ref: 'Spread'
+    },
+    cards: [{
+        type: Types.ObjectId,
+        ref: 'Card'
+    }],
+    userNotes: [{
+        noteTitle: {
+            type: String
+        },
+        textBody: {
+            type: String
+        }
+    }]
 });
 
 const Reading = model('Reading', readingSchema);
