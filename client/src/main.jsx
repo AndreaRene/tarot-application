139d790bf11d1be54fe4bf015252f3fc7b896ce9
tugwellchristi <tugwellchristi@gmail.com>
2024-02-24 14:05:21 -0800

    added router structure to main.jsx file and added error.jsx page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import App from './App.jsx'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Reading from './pages/Reading'
+import Signup from './pages/Signup'
+import UserDashboard from './pages/UserDashboard'
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Login',
+        element: <Login />,
+      },
+      {
+        path: '/Profile',
+        element: <Profile />,
+      },
+      {
+        path: '/Reading',
+        element: <Reading />,
+      },
+      {
+        path: '/Signup',
+        element: <Signup />,
+      },
+      {
+        path: '/UserDashboard',
+        element: <UserDashboard />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router} />
 )
