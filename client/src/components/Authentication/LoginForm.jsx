32abf8b58918e88ce9a5f56bbbbff2ffe53e9455
tugwellchristi <tugwellchristi@gmail.com>
2024-02-25 10:48:53 -0800

    fixed items per request in Github

--- client/src/components/Authentication/LoginForm.jsx
+++ client/src/components/Authentication/LoginForm.jsx
@@ -11,12 +11,12 @@ const LoginForm = () => {
                     <div className="m-7">
                         <form action="">
                             <div className="mb-6">
-                                <label for="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                                <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
                                 <input type="email" name="email" id="email" placeholder="you@email.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                             </div>
                             <div className="mb-6">
                                 <div className="flex justify-between mb-2">
-                                    <label for="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
+                                    <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
                                     <input type="password" name="password" id="password" placeholder="Your Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                                 </div>
                                 <a href="#!" className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
