c59da6b4dda9574f55612536f2f54591dccfcf26
tugwellchristi <tugwellchristi@gmail.com>
2024-02-25 11:45:54 -0800

    made suggested revision on signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
         <div className='flex items-center min-h-screen bg-white dark:bg-gray-900'>
             <div className='containe mx-auto'>
                 <div className='max-w-md mx-auto my-10'>
-                    <div class="m-7">
+                    <div className="m-7">
                         <SignUpForm />
                     </div>
                 </div>
