aa31816c0cceff7ee03bb6dfa5061a9f5295406f
tugwellchristi <tugwellchristi@gmail.com>
2024-02-25 00:42:41 -0800

    added a login form component, login page content, tailwind.config file, updated header file to hide when on login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LoginForm from '../components/Authentication/LoginForm';
+
+const Login = () => {
+    return (
+        <div className='flex items-center min-h-screen bg-white dark:bg-gray-900'>
+            <div className='containermx-auto'>
+                <div className='max-w-md mx-auto my-10'>
+                    <div class="m-7">
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
