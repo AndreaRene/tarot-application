e2915d3c4e322fc63abdd1715c7664fd80dc92f8
AndreaRene <andrea@interhact.net>
2024-02-26 14:44:51 -0600

    added birthday, readings, decks, and theme to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,45 +1,75 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model, Types } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    userName: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+
+  phoneNumber: {
+    type: String,
+  },
+
+  birthday: {
+    type: String,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+
+  useReverseCards: {
+    type: Boolean,
+    default: true,
+  },
+
+  readings: [
+    {
+      type: Types.ObjectId,
+      ref: "Reading",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must match an email address!'],
+  ],
+
+  decks: [
+    {
+      type: Types.ObjectId,
+      ref: "Deck",
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
-    useReverseCards: {
-        type: Boolean,
-        default: true,
-    },
+  ],
+
+  theme: {
+    type: String,
+    enum: ["Light", "Dark"],
+  },
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-    next();
+  next();
 });
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
