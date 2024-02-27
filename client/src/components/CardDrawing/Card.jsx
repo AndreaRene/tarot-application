4ec409208c573adebf2d4befe4701510cc99e934
tugwellchristi <tugwellchristi@gmail.com>
2024-02-26 18:46:00 -0800

    added an initial push for styling across the home.jsx, header.jsx, and login.jsx

--- client/src/components/CardDrawing/Card.jsx
+++ client/src/components/CardDrawing/Card.jsx
@@ -3,22 +3,27 @@ import Box from '@mui/material/Box';
 
 export default function BoxSystemProps() {
     return (
+        
         <Box
-        className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4"
-        height={300}
-        width={200}
-        my={4}
-        display='flex'
-        alignItems='center'
-        justifyContent='center'
-        gap={4}
-        p={3}
-        bg='indigo'
-        border='2px solid gold'
-        borderRadius='8px'
-        sx={{ backgroundColor: 'indigo' }}
+            className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 relative mx-auto lg:ml-4 md:ml-4 sm:ml-4"
+            height={300}
+            width={200}
+            my={4}
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+            gap={4}
+            p={6}
+            border='2px solid rgb(170, 142, 80)'
+            borderRadius='8px'
+            sx={{ backgroundColor: '#F0EDD8ff' }}
         >
-            Tarot Deck logo Here
+            <img
+                src='Splatter_transparent.png'
+                alt='Tarot Desk logo card'
+                className="inset-0 w-full h-full object-cover rounded-md"
+            />
         </Box>
     );
 }
+
