e0483b61e138930fe05b9d95ade75e8d19261f0b
Andrea <andrea@interhact.net>
2024-02-27 12:07:07 -0600

    created index files for RSW deck images for easy of access

---
+++ client/src/assets/tarot_cards/RyderWaiteSmith/Minor/Cups/index.js
@@ -0,0 +1,31 @@
+import Cups1 from './Cups01.jpg';
+import Cups2 from'./Cups02.jpg';
+import Cups3 from './Cups03.jpg';
+import Cups4 from './Cups04.jpg';
+import Cups5 from './Cups05.jpg';
+import Cups6 from './Cups06.jpg';
+import Cups7 from './Cups07.jpg';
+import Cups8 from './Cups08.jpg';
+import Cups9 from './Cups09.jpg';
+import Cups10 from './Cups10.jpg';
+import CupsPaige from './Cups11.jpg';
+import CupsKnight from './Cups12.jpg';
+import CupsQueen from './Cups13.jpg';
+import CupsKing from './Cups14.jpg';
+
+export {
+    Cups1,
+    Cups2,
+    Cups3,
+    Cups4,
+    Cups5,
+    Cups6,
+    Cups7,
+    Cups8,
+    Cups9,
+    Cups10,
+    CupsPaige,
+    CupsKnight,
+    CupsQueen,
+    CupsKing
+};
---
+++ client/src/assets/tarot_cards/RyderWaiteSmith/index.js
@@ -0,0 +1,9 @@
+import * as Major from './Major';
+import * as Minor from './Minor';
+import RWSCardBack from './RWSCardBack.jpg';
+
+export {
+    Major,
+    Minor,
+    RWSCardBack
+}
