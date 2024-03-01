3af3ee7d96d016c0cc6a5d814d0352cdf1cff38b
tugwellchristi <tugwellchristi@gmail.com>
2024-03-01 13:46:16 -0800

    added apollo server/client to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import './index.css';
 
-import App from './App.jsx'
-import Home from './pages/Home/Home.jsx'
-// import Login from './pages/Login.jsx'
-// import Profile from './pages/Profile.jsx'
-// import Reading from './pages/Reading.jsx'
-// import Signup from './pages/Signup.jsx'
-// import UserDashboard from './pages/UserDashboard.jsx'
-// import Error from './pages/Error.jsx'
+import App from './App.jsx';
+import Home from './pages/Home/Home.jsx';
+// import Login from './pages/Login.jsx';
+// import Profile from './pages/Profile.jsx';
+// import Reading from './pages/Reading.jsx';
+// import Signup from './pages/Signup.jsx';
+// import UserDashboard from './pages/UserDashboard.jsx';
+// import Error from './pages/Error.jsx';
+
+// Set up the Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Adjust this URI to match your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />,
+    // errorElement: <Error />, // Uncomment and import Error component as needed
     children: [
       {
         index: true,
@@ -47,5 +54,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <RouterProvider router={router} />
-)
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
