d19fd6e338edec1a529ac726237fed0442135bc1
tugwellchristi <tugwellchristi@gmail.com>
2024-03-01 14:30:28 -0800

    updated login form and modals, is functional

--- client/src/components/Authentication/LoginModal.jsx
+++ client/src/components/Authentication/LoginModal.jsx
@@ -1,46 +1,7 @@
 import { Modal } from '@mui/material';
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
-const LoginForm = () => {
-    return (
-        <div style={{ width: '300px', margin: 'auto' }}>
-            <Form id='loginForm'>
-                <h1
-                    className='text-bold'
-                    style={{
-                        color: 'rgb(170, 142, 80)',
-                        fontFamily: 'Amarante-Regular',
-                    }}
-                >
-                    Login
-                </h1>
-                <Form.Group
-                    className='mb-3 text-white'
-                    controlId='formBasicEmail'
-                >
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type='email' placeholder='Enter email' />
-                </Form.Group>
-                <Form.Group
-                    className='mb-3 text-white'
-                    controlId='formBasicPassword'
-                >
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' placeholder='Password' />
-                </Form.Group>
-                <Form.Text className='text-white'>
-                    Forget your password? Reset here.
-                </Form.Text>
-                <br />
-                <Button id='button' type='submit'>
-                    Submit
-                </Button>
-            </Form>
-        </div>
-    );
-};
+import LoginForm from './LoginForm';
+import PropTypes from 'prop-types'; // Import PropTypes
 
 const LoginModal = ({ open, handleClose }) => {
     return (
@@ -58,6 +19,11 @@ const LoginModal = ({ open, handleClose }) => {
             </div>
         </Modal>
     );
+};
+
+LoginModal.propTypes = {
+    open: PropTypes.bool.isRequired, // Validate open prop as a boolean (isRequired ensures the prop is provided)
+    handleClose: PropTypes.func.isRequired, // Validate handleClose prop as a function (isRequired ensures the prop is provided)
 };
 
 export default LoginModal;
