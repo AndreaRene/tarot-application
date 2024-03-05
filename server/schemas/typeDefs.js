3d7302d74a7a0aae66964eb4f088c96577d7b8e2
Andrea <andrea@interhact.net>
2024-03-04 18:21:14 -0600

    add mutation to add user decks

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -85,6 +85,10 @@ const typeDefs = `
         newPassword: String!
     }
 
+    input UpdateUserDeckInput {
+        decks: [ID]
+    }
+
     type Auth {
         token: ID!
         user: User
@@ -108,6 +112,7 @@ const typeDefs = `
         login(email: String!, password: String!): Auth
         updateUserProfile(userId: ID!, input: UpdateUserProfileInput):User
         updateUserPassword(userId: ID!, input: UpdateUserPasswordInput): User
+        updateUserDecks(userId: ID!, input: UpdateUserDecksInput): User
         logout: Logout!
         deleteUser: User
     }
