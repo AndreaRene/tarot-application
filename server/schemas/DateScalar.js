3b9e1164fa0583a141307aa6791a89b9b961011d
Andrea <andrea@interhact.net>
2024-03-07 19:27:05 -0600

    add defining file for data scalar

---
+++ server/schemas/DateScalar.js
@@ -0,0 +1,20 @@
+const { GraphQLScalarType, Kind } = require('graphql');
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  serialize(value) {
+    return value.toISOString();
+  },
+  parseValue(value) {
+    return new Date(value);
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.STRING) {
+      return new Date(ast.value);
+    }
+    return undefined;
+  },
+});
+
+module.exports = dateScalar;
