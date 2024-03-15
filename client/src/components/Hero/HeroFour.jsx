dad2ea7bc33a67173d66a18f4f217aab1ded8059
tugwellchristi <tugwellchristi@gmail.com>
2024-03-15 14:24:16 -0700

    added styling, dotStepper and text

--- client/src/components/Hero/HeroFour.jsx
+++ client/src/components/Hero/HeroFour.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../Hero/Assets/Hero.css';
 
-const HeroSectionFour = () => {
+const HeroSectionFour = ({ selectedDescription }) => {
     return (
         <div>
             <div>
                 <h1 className='text-white text-center mb-3 mt-5'>
-                    Feature descriptions listed here!
+                    {selectedDescription}
                 </h1>
             </div>
         </div>
