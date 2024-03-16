4a470b1c8425760866595673ed69d3a8bd55ffc5
AndreaRene <andrea@interhact.net>
2024-03-15 19:31:25 -0500

    create draw cards function

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -63,13 +63,19 @@ const checkAuthentication = (context, userId) => {
     }
 };
 
-function shuffleArray(array) {
+const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
+
+const drawCards = (deck, numberOfCards) => {
+  const shuffledDeck = shuffleArray([...deck]);
+  return shuffledDeck.slice(0, numberOfCards);
+}
+
 
 const resolvers = {
 
