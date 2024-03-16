51a3ed6f4f1dffbe24f35deca661b287db06cba0
AndreaRene <andrea@interhact.net>
2024-03-15 19:27:24 -0500

    create shuffle function to pass card id arrays through

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -63,6 +63,14 @@ const checkAuthentication = (context, userId) => {
     }
 };
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 const resolvers = {
 
     Date: dateScalar,
