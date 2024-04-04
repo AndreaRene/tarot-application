e89a5f3a446d77080063266a3bfebe2212a019c6
Andrea <andrea@interhact.net>
2024-04-04 08:03:12 -0500

    removed duplicated fields

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,6 @@ const { Schema, model, Types } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-
   username: {
     type: String,
     required: true,
@@ -13,7 +12,7 @@ const userSchema = new Schema({
     // Must start with a letter, letters can be uppercase or lowercase, can contain numbers and underscores
     validate: {
       validator: (value) => /^[A-Za-z][A-Za-z0-9_]{4,19}$/.test(value),
-      message: "Please choose a valid username.",
+      message: 'Please choose a valid username.',
     },
   },
 
@@ -23,20 +22,21 @@ const userSchema = new Schema({
     unique: true,
     validate: {
       validator: (value) => /.+@.+\..+/.test(value),
-      message: "Must match an email address!",
+      message: 'Must match an email address!',
     },
   },
 
   phoneNumber: {
     type: String,
     validate: {
-      validator: (value) => /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(value),
-      message: "Must be a valid phone number.",
+      validator: (value) =>
+        /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(value),
+      message: 'Must be a valid phone number.',
     },
   },
 
   fullName: {
-        type: String,
+    type: String,
   },
 
   password: {
@@ -46,70 +46,59 @@ const userSchema = new Schema({
     maxlength: 25,
     // Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character
     validate: {
-      validator: (value) => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,25}$/.test(value),
-      message: "Must be a valid password.",
+      validator: (value) =>
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,25}$/.test(
+          value
+        ),
+      message: 'Must be a valid password.',
     },
   },
+
+  birthday: {
+    type: Date,
+  },
+
   useReverseCards: {
     type: Boolean,
     default: true,
   },
 
-    birthday: {
-        type: Date,
+  readings: [
+    {
+      type: Types.ObjectId,
+      ref: 'Reading',
     },
+  ],
 
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-        // Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character maximum 25 characters
-        match: [
-            /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,25}$/,
-            'Must be a valid password.',
-        ],
+  decks: [
+    {
+      type: Types.ObjectId,
+      ref: 'Deck',
     },
-    useReverseCards: {
-        type: Boolean,
-        default: true,
-    },
+  ],
 
-    readings: [
-        {
-            type: Types.ObjectId,
-            ref: 'Reading',
-        },
-    ],
-
-    decks: [
-        {
-            type: Types.ObjectId,
-            ref: 'Deck',
-        },
-    ],
-
-    theme: {
-        type: String,
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now,
-    },
+  theme: {
+    type: String,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // set up pre-save middleware to create password
 userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-    next();
+  next();
 });
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 const User = model('User', userSchema);
