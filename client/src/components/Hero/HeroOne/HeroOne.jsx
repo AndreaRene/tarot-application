1be21778f32d0ab45055470fc9f719595accf2df
Andrea <andrea@interhact.net>
2024-04-16 10:01:32 -0500

    updates to hero section three for auth fix

--- client/src/components/Hero/HeroOne/HeroOne.jsx
+++ client/src/components/Hero/HeroOne/HeroOne.jsx
@@ -11,102 +11,104 @@ import '../../../App.css';
 import '../../../index.css';
 
 const HeroSectionOne = () => {
-  const [loginOpen, setLoginOpen] = useState(false);
-  const [signupOpen, setSignupOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const location = useLocation();
+    const [loginOpen, setLoginOpen] = useState(false);
+    const [signupOpen, setSignupOpen] = useState(false);
+    const location = useLocation();
+    const isLoggedIn = useAuth();
+    
+    const handleLoginOpen = () => setLoginOpen(true);
+    const handleLoginClose = () => setLoginOpen(false);
+    
+    const handleSignupOpen = () => setSignupOpen(true);
+    const handleSignupClose = () => setSignupOpen(false);
 
-  useEffect(() => {
-    setIsLoggedIn(useAuth());
-  }, []);
+    const handleLogout = () => {
+        useAuth.logout();
+    };
 
-  const handleLoginOpen = () => setLoginOpen(true);
-  const handleLoginClose = () => setLoginOpen(false);
+    const showDrawer = [
+        '/Home',
+        '/Dashboard',
+        '/Share',
+        '/Reading',
+        '/Journal',
+        '/Profile',
+        '/Community',
+        '/Spreads',
+        '/Decks',
+        '/Shop',
+        '/Faq',
+        '/Contact',
+        '/Legal',
+        '/About',
+    ].includes(location.pathname);
 
-  const handleSignupOpen = () => setSignupOpen(true);
-  const handleSignupClose = () => setSignupOpen(false);
+    useEffect(() => {
+        if (isLoggedIn.loggedIn) {
+            setLoginOpen(false);
+            setSignupOpen(false);
+        }
+    }, [isLoggedIn.loggedIn]);
 
-  const handleLogout = () => {
-    useAuth.logout();
-    setIsLoggedIn(false);
-  };
+    return (
+        <div className='hero-section-one'>
+            <div className='text-center mb-2 mt-4'>
+                <div>
+                    <Link
+                        to='/'
+                        style={{
+                            position: 'relative',
+                            zIndex: 3,
+                        }}
+                    >
+                        <img
+                            src='tarot_logo.png'
+                            alt='Tarot Deck Logo'
+                            style={{
+                                width: '130px',
+                                marginTop: '20px',
+                                marginBottom: '30px',
+                                zIndex: '3',
+                            }}
+                        />
+                    </Link>
+                </div>
+                <img
+                    src='/Up_moon.png'
+                    alt='Moon decorative element'
+                    className='moon-up img-fluid img-sm'
+                />
+                <h1 className='hero-one-font font-bold text-center mt-1 display-2 display-md-5 display-lg-6 mb-3'>
+                    Embark on an Enlightening <br /> Journey with Tarot
+                </h1>
+                <img
+                    src='/Down_moon.png'
+                    alt='Moon decorative element'
+                    className='mb-5 img-fluid img-sm'
+                />
+                <div className='nav-buttons-one'>
+                    {isLoggedIn.loggedIn ? (
+                        <AccountCircleIcon sx={{ fontSize: 56, marginBottom: '35px' }} />
+                    ) : (
+                        <>
+                            <button className='nav-button mb-5' onClick={handleLoginOpen}>
+                                Login
+                            </button>
+                            <button className='nav-button mb-5' onClick={handleSignupOpen}>
+                                Sign Up
+                            </button>
+                        </>
+                    )}
+                </div>
 
-  const showDrawer = [
-    '/Home',
-    '/Dashboard',
-    '/Share',
-    '/Reading',
-    '/Journal',
-    '/Profile',
-    '/Community',
-    '/Spreads',
-    '/Decks',
-    '/Shop',
-    '/Faq',
-    '/Contact',
-    '/Legal',
-    '/About',
-  ].includes(location.pathname);
-
-  return (
-    <div className='hero-section-one'>
-      <div className='text-center mb-2 mt-4'>
-        <div>
-          <Link
-            to='/'
-            style={{
-              position: 'relative',
-              zIndex: 3,
-            }}
-          >
-            <img
-              src='tarot_logo.png'
-              alt='Tarot Deck Logo'
-              style={{
-                width: '130px',
-                marginTop: '20px',
-                marginBottom: '30px',
-                zIndex: '3',
-              }}
-            />
-          </Link>
+                <LoginModal open={loginOpen} handleClose={handleLoginClose} />
+                <SignupModal open={signupOpen} handleClose={handleSignupClose} />
+                {showDrawer && (
+                    <Drawer isLoggedIn={isLoggedIn.loggedIn} handleLogout={handleLogout} />
+                )}
+            </div>
         </div>
-        <img
-          src='/Up_moon.png'
-          alt='Moon decorative element'
-          className='moon-up img-fluid img-sm'
-        />
-        <h1 className='hero-one-font font-bold text-center mt-1 display-2 display-md-5 display-lg-6 mb-3'>
-          Embark on an Enlightening <br /> Journey with Tarot
-        </h1>
-        <img
-          src='/Down_moon.png'
-          alt='Moon decorative element'
-          className='mb-5 img-fluid img-sm'
-        />
-        <div className='nav-buttons-one'>
-          {isLoggedIn ? (
-            <AccountCircleIcon sx={{ fontSize: 56, marginBottom: '35px' }} />
-          ) : (
-            <>
-              <button className='nav-button mb-5' onClick={handleLoginOpen}>
-                Login
-              </button>
-              <button className='nav-button mb-5' onClick={handleSignupOpen}>
-                Sign Up
-              </button>
-            </>
-          )}
-        </div>
-
-        <LoginModal open={loginOpen} handleClose={handleLoginClose} />
-        <SignupModal open={signupOpen} handleClose={handleSignupClose} />
-        {showDrawer && (
-          <Drawer isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HeroSectionOne;
