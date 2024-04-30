ddcbe114cd403186e6cb54437e29ca81b9e307ce
tugwellchristi <tugwellchristi@gmail.com>
2024-04-30 14:33:52 -0700

    added content to newReading and Shop pages

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,23 +1,9 @@
-// import React from 'react';
-// import './Shop.css';
-// import Header from '../../components/Header/Header';
-// import Drawer from '../../components/Drawer/Drawer';
+const AppShop = () => {
+    return (
+<section>
+    <p>Exciting products coming soon!</p>
+</section>
+    );
+};
 
-// const Shop = ({ isLoggedIn }) => {
-//     return (
-//         <div className='shop-container'>
-//             <Header />
-//             {isLoggedIn && (
-//                 <div className='drawer-container'>
-//                     <Drawer />
-//                 </div>
-//             )}
-//             <div className='intro-shop'>
-//                 <h1 className='welcome'>App Store</h1>
-//                 <p>Welcome to our App Store </p>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Shop;
+export default AppShop;
