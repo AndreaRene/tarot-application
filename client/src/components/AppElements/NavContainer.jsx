4c6931c32b07e37a3bbaad60d57156acb1b54d2d
AndreaRene <andrea@interhact.net>
2024-05-01 14:45:17 -0500

    update NavContainer for dynamic loading based on auth

--- client/src/components/AppElements/NavContainer.jsx
+++ client/src/components/AppElements/NavContainer.jsx
@@ -1,52 +1,32 @@
 import { useLocation } from 'react-router-dom';
-import NavLinks from '../NavigationPane/NaviationLinks';
+import { useAuth } from '../../utils/auth/AuthContext';
 import NavIcons from '../NavigationPane/NavigationIcons';
-
-const routeToNavComponents = {
-    '/dashboard': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    ),
-    '/profile': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    ),
-    '/newreading': NavIcons,
-    '/community': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    ),
-    '/browseSpreads': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    ),
-    '/browseDecks': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    ),
-    '/appShop': () => (
-        <div style={ { display: 'flex', height: '100%' } }>
-            <NavLinks />
-            <NavIcons />
-        </div>
-    )
-};
+import NavLinks from '../NavigationPane/NavigationLinks';
 
 const NavContainer = () => {
     const location = useLocation();
-    const NavComponents = routeToNavComponents[location.pathname];
+    const { isAuthenticated } = useAuth();
+  
+    const getNavComponent = () => {
+      const commonNav = (
+        <div style={{ display: 'flex', height: '100%' }}>
+          <NavLinks />
+          <NavIcons />
+        </div>
+      );
+  
+      // Mapping routes to components
+      const routes = {
 
-    return <div style={ { display: 'flex' } }>{ NavComponents && <NavComponents /> }</div>;
-};
-
-export default NavContainer;
+      };
+  
+      // Fallback for routes not explicitly defined
+      return routes[location.pathname] || null;
+    };
+  
+    const NavComponent = getNavComponent();
+  
+    return <div style={{ display: 'flex' }}>{NavComponent}</div>;
+  };
+  
+  export default NavContainer;
--- client/src/utils/authService.js
+++
@@ -1,9 +0,0 @@
-export async function login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/reading');
-}
-
-export function logout() {
-    localStorage.removeItem('id_token');
-    window.location.assign('/landing')
-}
