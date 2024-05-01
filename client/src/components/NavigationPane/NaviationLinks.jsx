fe62efdc5d013669b7d6cda443af4ad2d47be3f1
AndreaRene <andrea@interhact.net>
2024-05-01 09:11:38 -0500

    updated auth filename to keep consistency in frontend conventions

--- client/src/components/NavigationPane/NaviationLinks.jsx
+++ client/src/components/NavigationPane/NaviationLinks.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { logout } from '../../utils/auth';
+import { logout } from '../../utils/AuthContext';
 
 const NavigationLinks = () => {
     return (
