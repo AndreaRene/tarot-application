38e8349c47c3c680883994948a1a1c2d81c134d3
AndreaRene <andrea@interhact.net>
2024-05-03 12:18:45 -0500

    initial component creation for left side of card info page

---
+++ client/src/pages/CardDetails/CardLeft.jsx
@@ -0,0 +1,16 @@
+import CardFront from '../../components/CardComponents/TallRectangleCards/CardFront';
+
+const CardLeft = () => {
+return (
+    <section>
+        <h2>
+            The Title of the Card
+        </h2>
+        <h3>
+            The deck the card is from
+        </h3>
+    </section>
+)
+}
+
+export default CardLeft
