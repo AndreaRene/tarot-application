187617289653cd47290c5f1827f1e0bcd9dfdc2e
AndreaRene <andrea@interhact.net>
2024-05-07 11:52:32 -0500

    update component render in maincontainer file

--- client/src/components/AppElements/MainContainer.jsx
+++ client/src/components/AppElements/MainContainer.jsx
@@ -44,7 +44,7 @@ const routeToMainComponents = {
   // This is a temporary link in the navigation pane for testing purposes only. It will be removed. Access to the page will be determined by chose card
   '/cardDetails': () => {
     <section style={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' } }>
-      <SettingsLeft style={ { width: '50%' } } />
+      <CardLeft style={ { width: '50%' } } />
       <CardRight style={ { width: '50%' } } />
     </section>
   },
