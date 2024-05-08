4da5445caec72811070c6e637ff255c1d65d2ddd
AndreaRene <andrea@interhact.net>
2024-05-07 21:17:48 -0500

    fix scrollbar styling for entire app

--- client/src/pages/CardDetails/CardLeft.jsx
+++ client/src/pages/CardDetails/CardLeft.jsx
@@ -1,6 +1,6 @@
 const CardLeft = () => {
     return (
-        <section style={ { width: '50%', height: '95%', display: 'flex', justifyContent: 'center', alignItems: 'center' } }>
+        <section style={ { width: '50%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' } }>
             <section style={ { display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '65%', textAlign: 'center', padding: '5px' } }>
                 <div>
                     <h2>The Title of the Card</h2>
@@ -8,8 +8,8 @@ const CardLeft = () => {
                 </div>
 
                 <hr />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi nemo commodi modi rerum dolorem, aspernatur nobis ut ipsam eos reiciendis accusamus repellat vel corrupti pariatur officia consequatur reprehenderit amet.Qui minus adipisci itaque harum consequatur. Accusamus voluptatum earum pariatur culpa id minus eligendi inventore rerum quasi architecto fugit expedita veniam dolores itaque perferendis magni reiciendis alias ut, atque esse.</p>
-                <p>Soluta, cum ipsam! Fugit perspiciatis error sapiente exercitationem expedita rerum incidunt laboriosam numquam ad magnam, aspernatur at corporis, quam corrupti aperiam qui autem nam rem dolore laudantium quos eveniet? Atque.</p>
+                <p style={ { textAlign: 'justify' } }>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi nemo commodi modi rerum dolorem, aspernatur nobis ut ipsam eos reiciendis accusamus repellat vel corrupti pariatur officia consequatur reprehenderit amet.Qui minus adipisci itaque harum consequatur. Accusamus voluptatum earum pariatur culpa id minus eligendi inventore rerum quasi architecto fugit expedita veniam dolores itaque perferendis magni reiciendis alias ut, atque esse.</p>
+                <p style={ { textAlign: 'justify' } }>Soluta, cum ipsam! Fugit perspiciatis error sapiente exercitationem expedita rerum incidunt laboriosam numquam ad magnam, aspernatur at corporis, quam corrupti aperiam qui autem nam rem dolore laudantium quos eveniet? Atque.</p>
                 <hr />
 
                 <h2 style={ { backgroundColor: '#382337', display: 'inline-block', padding: '.5rem', margin: '20px' } }>
@@ -18,8 +18,8 @@ const CardLeft = () => {
                     <i className='fas fa-angle-right fa-lg'></i>
                 </h2>
                 <section style={ { width: '90%', display: 'grid', gridTemplateColumns: '1fr 2fr' } }>
-                    <div style={ { padding: '20px', borderRight: '1px solid white' } }>
-                        <h4>Keywords:</h4>
+                    <div style={ { borderRight: '1px solid white' } }>
+                        <h4 style={ { textAlign: 'left' } }>Keywords:</h4>
                         <ul style={ { textAlign: 'left' } }>
                             <li>Lorem </li>
                             <li>Ipsum</li>
@@ -27,9 +27,9 @@ const CardLeft = () => {
                             <li>Sit amet</li>
                         </ul>
                     </div>
-                    <div style={ { padding: '5px', height: '12rem', overflowX: 'hidden', overflowY: 'scroll' } }>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi nemo commodi modi rerum dolorem, aspernatur nobis ut ipsam eos reiciendis accusamus repellat vel corrupti pariatur officia consequatur reprehenderit amet.Qui minus adipisci itaque harum consequatur. </p>
-                        <p>Soluta, cum ipsam! Fugit perspiciatis error sapiente exercitationem expedita rerum incidunt laboriosam numquam ad magnam, aspernatur at corporis, quam corrupti aperiam qui autem nam rem dolore laudantium quos eveniet? Atque.</p>
+                    <div style={ { padding: '0 10px', height: '12rem', overflowX: 'hidden', overflowY: 'scroll' } }>
+                        <p style={ { textAlign: 'justify' } }>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi nemo commodi modi rerum dolorem, aspernatur nobis ut ipsam eos reiciendis accusamus repellat vel corrupti pariatur officia consequatur reprehenderit amet.Qui minus adipisci itaque harum consequatur. </p>
+                        <p style={ { textAlign: 'justify' } }>Soluta, cum ipsam! Fugit perspiciatis error sapiente exercitationem expedita rerum incidunt laboriosam numquam ad magnam, aspernatur at corporis, quam corrupti aperiam qui autem nam rem dolore laudantium quos eveniet? Atque.</p>
                         <p>Second Column Content</p>
                     </div>
                 </section>
