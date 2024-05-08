8e30466b193f0d2be0fea1e6a2786cecebc084a0
AndreaRene <andrea@interhact.net>
2024-05-07 20:52:22 -0500

    start of card left component with title, deck, and description

--- client/src/pages/CardDetails/CardLeft.jsx
+++ client/src/pages/CardDetails/CardLeft.jsx
@@ -1,10 +1,24 @@
 const CardLeft = () => {
     return (
-        <section style={ { width: '50%', display: 'flex', justifyContent: 'center', borderLeft: '1px solid lightgrey' } }>
-            <section style={ { width: '65%', textAlign: 'center', padding: '5px' } }>
-                <h2>The Title of the Card</h2>
-                <h3>The deck the card is from</h3>
+        <section style={ { width: '50%', height: '95%', display: 'flex', justifyContent: 'center', alignItems: 'center' } }>
+            <section style={ { display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '65%', textAlign: 'center', padding: '5px' } }>
+                <div>
+                    <h2>The Title of the Card</h2>
+                    <h3>The deck the card is from</h3>
+                </div>
+
                 <hr />
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi nemo commodi modi rerum dolorem, aspernatur nobis ut ipsam eos reiciendis accusamus repellat vel corrupti pariatur officia consequatur reprehenderit amet.Qui minus adipisci itaque harum consequatur. Accusamus voluptatum earum pariatur culpa id minus eligendi inventore rerum quasi architecto fugit expedita veniam dolores itaque perferendis magni reiciendis alias ut, atque esse.</p>
+                <p>Soluta, cum ipsam! Fugit perspiciatis error sapiente exercitationem expedita rerum incidunt laboriosam numquam ad magnam, aspernatur at corporis, quam corrupti aperiam qui autem nam rem dolore laudantium quos eveniet? Atque.</p>
+                <hr />
+
+                <h2 style={ { backgroundColor: '#382337', display: 'inline-block', padding: '.5rem', margin: '20px' } }>
+                    <i className='fas fa-angle-left fa-lg'></i>
+                    <span style={ { backgroundColor: '#4F334A', padding: '.5rem' } }>Upright</span>
+                    <i className='fas fa-angle-right fa-lg'></i>
+                </h2>
+
+
             </section>
         </section>
     )
