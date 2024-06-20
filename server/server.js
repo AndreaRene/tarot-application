e30e8abec82976da5b2fb2caad7033cc7fdf0cdf
AndreaRene <andrea@interhact.net>
2024-06-20 12:58:22 -0500

    added sdk setup for s3 buckets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-const AWS = require('aws-sdk');
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const AWS = require("aws-sdk");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,10 +24,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/dist as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
 
@@ -42,7 +42,7 @@ const startApolloServer = async () => {
     await server.start();
     server.applyMiddleware({ app });
 
-    db.once('open', () => {
+    db.once("open", () => {
       app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
