d6d7785534fa7ded93f1774d715816940cc1fe2a
Joseph <josephpicardat1@gmail.com>
2024-09-03 23:16:21 -0500

    feat(BrowseDecks): Added data to the decks [TDS-152]

--- client/src/pages/BrowseDecks/BrowseDecks.jsx
+++ client/src/pages/BrowseDecks/BrowseDecks.jsx
@@ -1,9 +1,12 @@
-import { useState, forwardRef, cloneElement } from 'react';
+import { useState, forwardRef, cloneElement, useEffect } from 'react';
 import { Button, Modal } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useSpring, animated } from '@react-spring/web';
 import DecksModal from './DecksModal';
 import tarot_board from '../../assets/tarot_board.png';
+
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_ALL_DECKS } from '../../utils/queries';
 
 import './BrowseDecks.css';
 
@@ -44,24 +47,107 @@ Fade.propTypes = {
 
 const BrowseDecks = () => {
     const [open, setOpen] = useState(false); // Toggles Spread Modal
+    const [allDecks, { data: allDecksData }] = useLazyQuery(QUERY_ALL_DECKS);
 
-    const handleOpen = () => setOpen(true);
+    const [deckInfo, setDeckInfo] = useState({});
+    const [modalData, setModalData] = useState({
+        deckName: '',
+        deckDescription: '',
+        imageUrl: ''
+    });
+
+    useEffect(() => {
+        if (allDecksData) {
+            const formattedDecks = allDecksData.allDecks.reduce((acc, deck) => {
+                acc[deck.deckId] = {
+                    deckName: deck.deckName,
+                    deckDescription: deck.deckDescription,
+                    imageUrl: deck.imageUrl
+                };
+                return acc;
+            }, {});
+
+            setDeckInfo(formattedDecks);
+        }
+    }, [allDecksData]);
+
+    useEffect(() => {
+        allDecks();
+    }, [allDecks]);
+
+    const handleOpen = (deck) => {
+        setModalData(deck);
+        setOpen(true);
+    };
     const handleClose = () => setOpen(false);
+
+    const DeckMap = () => {
+        const totalDecks = 5;
+        const deckIds = Object.keys(deckInfo);
+        const numDecksToShow = Math.min(deckIds.length, totalDecks);
+        const numComingSoon = totalDecks - numDecksToShow;
+
+        return (
+            <div className='deckContainer'>
+                {deckIds.slice(0, numDecksToShow).map((deckId) => (
+                    <img
+                        key={deckId}
+                        className='deckImgs'
+                        alt={deckInfo[deckId].deckName}
+                        src={deckInfo[deckId].imageUrl}
+                        onClick={() =>
+                            handleOpen({
+                                deckName: deckInfo[deckId].deckName,
+                                deckDescription: deckInfo[deckId].deckDescription,
+                                imageUrl: deckInfo[deckId].imageUrl
+                            })
+                        }
+                    />
+                ))}
+
+                {Array.from({ length: numComingSoon }).map((_, idx) => (
+                    <div
+                        key={`coming-soon-${idx}`}
+                        className='deckImgs comingSoon'>
+                        <h2 className='comingSoonTitle'>COMING SOON</h2>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const FeaturedDeck = () => {
+        if (!deckInfo.RWSD) {
+            // Optionally, you can return a placeholder or loading spinner here
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div
+                    style={{ backgroundImage: `url(${tarot_board})` }}
+                    className='featuredDeckImgContainer'>
+                    <img
+                        className='featuredDeckImg'
+                        alt={deckInfo.EOTS.deckName}
+                        src={deckInfo.EOTS.imageUrl}
+                        onClick={() =>
+                            handleOpen({
+                                deckName: deckInfo.EOTS.deckName,
+                                deckDescription: deckInfo.EOTS.deckDescription,
+                                imageUrl: deckInfo.EOTS.imageUrl
+                            })
+                        }
+                    />
+                </div>
+            );
+        }
+    };
 
     return (
         <section className='decksWrapper'>
             <div className='topSection'>
                 <div className='deckLeftSide'>
                     <h3 className='featuredDeckTitle'>Featured Deck: Eclipse of the Soul</h3>
-                    <div
-                        style={{ backgroundImage: `url(${tarot_board})` }}
-                        className='featuredDeckImgContainer'>
-                        <img
-                            className='featuredDeckImg'
-                            alt='eclipseOfTheSoul'
-                            onClick={handleOpen}
-                        />
-                    </div>
+                    <FeaturedDeck />
                 </div>
                 <div className='deckRightSide'>
                     <div className='rightSideInsideContainer'>
@@ -78,33 +164,7 @@ const BrowseDecks = () => {
             </div>
             <div className='bottomSection'>
                 <Button className='arrow'>❮</Button>
-                <div className='deckContainer'>
-                    <img
-                        className='deckImgs'
-                        alt='deck1'
-                        onClick={handleOpen}
-                    />
-                    <img
-                        className='deckImgs'
-                        alt='deck2'
-                        onClick={handleOpen}
-                    />
-                    <img
-                        className='deckImgs'
-                        alt='deck3'
-                        onClick={handleOpen}
-                    />
-                    <img
-                        className='deckImgs'
-                        alt='deck4'
-                        onClick={handleOpen}
-                    />
-                    <img
-                        className='deckImgs'
-                        alt='deck5'
-                        onClick={handleOpen}
-                    />
-                </div>
+                <DeckMap />
                 <Button className='arrow'>❯</Button>
             </div>
 
@@ -114,7 +174,12 @@ const BrowseDecks = () => {
                 aria-labelledby='modal-modal-title'
                 aria-describedby='modal-modal-description'>
                 <Fade in={open}>
-                    <DecksModal onClose={handleClose} />
+                    <DecksModal
+                        onClose={handleClose}
+                        deckName={modalData.deckName}
+                        deckDescription={modalData.deckDescription}
+                        imageUrl={modalData.imageUrl}
+                    />
                 </Fade>
             </Modal>
         </section>
