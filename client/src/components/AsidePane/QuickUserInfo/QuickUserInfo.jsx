7aa7f8568ca7c86cd7acbf5f72fbdc65ededb7b4
Joseph <josephpicardat1@gmail.com>
2024-09-25 19:05:26 -0500

    fix(QuickUserInfo): avatar now displays correctly

--- client/src/components/AsidePane/QuickUserInfo/QuickUserInfo.jsx
+++ client/src/components/AsidePane/QuickUserInfo/QuickUserInfo.jsx
@@ -63,7 +63,7 @@ const QuickUserInfo = () => {
     const [avatar, setAvatar] = useState(preferences.avatar);
 
     useEffect(() => {
-        setAvatar(preferences.avatar);
+        setAvatar(preferences.avatar.imageUrl);
     }, [preferences.avatar]);
 
     return (
